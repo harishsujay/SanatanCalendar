af24b120af37bd6d3460189e869ceafca7daf1cd
Harish Setty <chanderharish1@gmail.com>
2016-05-20 19:12:49 +1000

    B1 - Creation of MVC group and adding files to the appropriate group

---
+++ SanatanHinduCalendar/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  SanatanHinduCalendar
+//
+//  Created by Harish Setty on 20/05/2016.
+//  Copyright © 2016 RMIT. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class Model {
+
+    
+}
